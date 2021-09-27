d9392a3afdb3022f171004cdea37a49b503ca8fb
foxtail88 <foxtail88@example.com>
2021-09-27 16:09:03 +0900

    p.15: change return type to pointer

--- money/money.go
+++ money/money.go
@@ -4,6 +4,6 @@ type Dollar struct {
 	amount int
 }
 
-func (d *Dollar) times(multiplier int) Dollar {
-	return Dollar{d.amount * multiplier}
+func (d *Dollar) times(multiplier int) *Dollar {
+	return &Dollar{d.amount * multiplier}
 }
