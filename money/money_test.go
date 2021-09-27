436561f3f16209320d50620098368f07ef155940
foxtail88 <foxtail88@example.com>
2021-09-27 16:07:03 +0900

    p.15

--- money/money_test.go
+++ money/money_test.go
@@ -4,8 +4,12 @@ import "testing"
 
 func TestMoney(t *testing.T) {
 	five := Dollar{5}
-	five.times(2)
-	if five.amount != 10 {
+	product := five.times(2)
+	if product.amount != 10 {
 		t.Errorf("expected 10, but answer is %d", five.amount)
+	}
+	product = five.times(3)
+	if product.amount != 15 {
+		t.Errorf("expected 15, but answer is %d", five.amount)
 	}
 }
